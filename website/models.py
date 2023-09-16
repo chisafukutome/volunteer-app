f52cc869ed3f9556d3d03509948cac6f046c2d9e
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2023-09-16 03:23:46 -0400

    Update Models

--- website/models.py
+++ website/models.py
@@ -1,6 +1,31 @@
-#DB tables
+# DB tables
 from . import db
+
 
 class User(db.model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=False)
     email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String, nullable=False)
+
+
+class Event(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    u_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    host_name = db.Column(db.String, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    hours = db.Column(db.Integer, nullable=False)
+
+
+class Sponsor(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    u_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    e_id = db.Column(db.Integer, db.ForeignKey("Event.id"), nullable=False)
+
+
+class Volunteering(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    u_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    e_id = db.Column(db.Integer, db.ForeignKey("Event.id"), nullable=False)
+    hours_worked = db.Column(db.Integer, nullable=False)
