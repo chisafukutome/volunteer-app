0b847daefe2f9169492db77f037e6388ac218bf1
chisa-f <kiki4537fer2cat@gmail.com>
2023-09-16 04:12:56 -0400

    Edit db

--- website/models.py
+++ website/models.py
@@ -1,36 +1,36 @@
 #DB tables
 from . import db
 
-class User(db.model):
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=False)
     email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String, nullable=False)
-    credit_hours = db.Column(db.Integer, nullable=False)
-    total_hours = db.Column(db.Integer, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    credit_hours = db.Column(db.Integer, nullable=True)
+    total_hours = db.Column(db.Integer, nullable=True)
 
 
-class Event(db.model):
+class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    u_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    host_name = db.Column(db.String, nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    start_date = db.Column(db.Date, nullable=False)
-    end_date = db.Column(db.Date, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    host_name = db.Column(db.String(150), nullable=False)
+    start_date = db.Column(db.DateTime(timezone=True), nullable=False)
+    end_date = db.Column(db.DateTime(timezone=True), nullable=False)
     description = db.Column(db.String(200), nullable=False)
     hours = db.Column(db.Integer, nullable=False)
-    vol_need = db.Column(db.Integer, nullable=False)
+    credits_need = db.Column(db.Integer, nullable=False)
 
 
-class Sponsor(db.model):
+class Prize(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    u_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    e_id = db.Column(db.Integer, db.ForeignKey("Event.id"), nullable=False)
-    hour_need = db.Column(db.Integer, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey("event.id"), nullable=False)
+    hours_need = db.Column(db.Integer, nullable=True)
 
 
-class Volunteering(db.model):
+class Volunteer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    u_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    e_id = db.Column(db.Integer, db.ForeignKey("Event.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey("Event.id"), nullable=False)
     hours_worked = db.Column(db.Integer, nullable=False)
