2ed2155b370fd2414d574fc02293c3286650ccf6
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2023-09-16 03:35:52 -0400

    Update models.py

--- website/models.py
+++ website/models.py
@@ -7,21 +7,24 @@ class User(db.model):
     name = db.Column(db.String, unique=False)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String, nullable=False)
+    credit_hours = db.Column(db.Integer, nullable=False)
+    total_hours = db.Column(db.Integer, nullable=False)
 
 
 class Event(db.model):
     id = db.Column(db.Integer, primary_key=True)
     u_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     host_name = db.Column(db.String, nullable=False)
-    date = db.Column(db.Date, nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
     description = db.Column(db.String(200), nullable=False)
-    hours = db.Column(db.Integer, nullable=False)
+    vol_need = db.Column(db.Integer, nullable=False)
 
 
 class Sponsor(db.model):
     id = db.Column(db.Integer, primary_key=True)
     u_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    e_id = db.Column(db.Integer, db.ForeignKey("Event.id"), nullable=False)
+    hour_need = db.Column(db.Integer, nullable=True)
 
 
 class Volunteering(db.model):
@@ -29,3 +32,4 @@ class Volunteering(db.model):
     u_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     e_id = db.Column(db.Integer, db.ForeignKey("Event.id"), nullable=False)
     hours_worked = db.Column(db.Integer, nullable=False)
+
