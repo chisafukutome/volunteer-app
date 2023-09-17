9e53106fcf135e38db02b6647ae42aa202625ab0
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2023-09-16 23:40:57 -0400

    Updates

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 #codes related to pages before authentication
 from flask import Blueprint, render_template, redirect, url_for
 from .models import Event
+from datetime import datetime
 
 views = Blueprint("views", __name__)
 
@@ -9,5 +10,8 @@ views = Blueprint("views", __name__)
 def home():
     # Select all of events
     events = Event.query.all()
+    for event in events:
+        event1 = datetime.strptime(event.start_date, "%Y-%m-%dT%H:%M")
+        event.start_date = event1.strftime("%b %d, %Y")
     return render_template("home.html", events=events)
 
